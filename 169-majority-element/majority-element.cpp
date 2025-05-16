6765a97bf8e3d176fa214e4984e04f3760c3bd61
Mayur <153451278+mayursoni5@users.noreply.github.com>
2025-05-16 12:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (58.25%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int el = 0;
+        int cnt = 0;
         int n = nums.size();
-        map<int, int> mpp;
-
-        for (int i = 0; i < n; i++) {
-            mpp[nums[i]]++;
-        }
-        for (auto it : mpp) {
-            if (it.second > (n / 2)) {
-                return it.first;
+        for(int i = 0; i < n ; i++){
+            if(cnt == 0){
+                el = nums[i];
+                cnt++;
             }
+            else if(el == nums[i]) cnt++;
+            else cnt--;
         }
-        return -1;
+        return el;
     }
 };
